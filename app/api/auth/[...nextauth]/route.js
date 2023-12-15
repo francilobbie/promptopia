81b760011c50ecf9b067070d81c63f1d3b65b306
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2023-12-15 06:24:05 +0100

    init app

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser?._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        let user = await User.findOne({ email: profile.email });
+
+        if (!user) {
+          // Create a username that adheres to your schema's requirements
+          const username = profile.email.split("@")[0].replace(/[^a-zA-Z0-9._]/g, "").substring(0, 20);
+          user = await User.create({
+            email: profile.email,
+            username: username,
+            image: profile.picture
+          });
+        }
+
+        // Additional checks or operations can be done here
+
+        return true; // Sign-in successful
+      } catch (error) {
+        console.error("Error in signIn callback", error);
+        // Handle specific errors differently if needed
+        return false; // Deny access on error
+      }
+    }
+  }
+});
+
+export { handler as GET, handler as POST };
