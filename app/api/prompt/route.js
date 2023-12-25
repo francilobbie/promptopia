86f2337585d32b1fd282686061b48a09b4d497ad
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2023-12-25 19:43:34 +0100

    trying to avoid caching problem

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,24 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (req) => {
+export const GET = async () => {
   try {
     await connectToDB();
-
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const response = new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, max-age=0',
+      },
+    });
+    return response;
   } catch (error) {
-    return new Response("Failed to fetch prompts", { status: 500 });
+    console.error("Error fetching prompts:", error);
+    return new Response(JSON.stringify({ message: "Failed to fetch prompts" }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
-}
+};
