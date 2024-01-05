464185d6482afd6e8a5f52a82d8472fd35eb8ffc
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2024-01-05 21:16:47 +0100

    Trying to fix app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,37 @@
+// import '@styles/globals.css'
+// import Head from 'next/head'
+// import Nav from '@components/Nav'
+// import Provider from '@components/Provider'
+
+// export const metadata = {
+//   title: "Promptopia",
+//   description: "Discover & Share AI prompts"
+// }
+
+// const RootLayout = ({ children }) => {
+//   return (
+//     <>
+//       <Head>
+//         <title>{metadata.title}</title>
+//         <meta name="description" content={metadata.description} />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <Provider>
+//         <div className='main'>
+//           <div className='gradient' />
+//         </div>
+
+//         <main className='app'>
+//           <Nav />
+//           {children}
+//         </main>
+//       </Provider>
+//     </>
+//   )
+// }
+
+export default RootLayout
 import '@styles/globals.css'
 import Head from 'next/head'
 import Nav from '@components/Nav'
@@ -15,18 +49,20 @@ const RootLayout = ({ children }) => {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href="/favicon.ico" />
+        <html lang="en" />
       </Head>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
 
-      <Provider>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
-      </Provider>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </>
   )
 }
